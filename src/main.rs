7ef655d2381aa4bf141b9edb182ed6dc5d9a67c6
Ubuntu <mahesh.ksl@gmail.com>
2022-08-24 12:08:00 +0000

    tcp code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    println!("$$$$$$$$$$$$$$$$$$$$$$$$$");
+    let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
+

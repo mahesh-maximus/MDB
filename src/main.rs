f081382401a8856266a48bb58bbacf3dc5439e70
Ubuntu <mahesh.ksl@gmail.com>
2022-12-05 16:09:17 +0000

    added Tungstenite

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,40 @@
 use std::{
     fs,
-    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
     thread,
     time::Duration,
     str,
     io::{Read, Result, Write}
 };
-use sha1::{Sha1, Digest};
-use base64::{encode, decode};
+use std::thread::spawn;
+use tungstenite::accept;
 
 fn main() {
 
     println!("Starting MDB ...");
 
     thread::spawn(|| {
-        let listener = TcpListener::bind("0.0.0.0:8000").unwrap();
+      //  let listener = TcpListener::bind("0.0.0.0:8000").unwrap();
 
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
+    let server = TcpListener::bind("0.0.0.0:8000").unwrap();
+    for stream in server.incoming() {
+        spawn (move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            loop {
+                let msg = websocket.read_message().unwrap();
 
-            println!("WS connection");
+                // We do not want to send back ping/pong messages.
+                if msg.is_binary() || msg.is_text() {
+                    websocket.write_message(msg).unwrap();
+                }
+            }
+        });
+    }
 
-            process(stream);
-        }
-    });
+   });
 
+
+    println!("Starting WS");
 
     let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
 
@@ -88,50 +97,5 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write_all(response.as_bytes()).unwrap();
     stream.flush().unwrap();  
-}
-
-fn process(mut stream: TcpStream) -> Result<()> {
-    let mut buf = [0; 80 * 1024];
-    let len = stream.read(&mut buf)?;
-    let data = &buf[..len];
-
-    if data.starts_with(b"GET / HTTP/1.1") {
-        let req = String::from_utf8(data.to_vec()).unwrap();
-
-        println!("Received:\n\n{req}");
-
-        let sec_key = req
-            .lines()
-            .filter_map(|line| line.split_once(": "))
-            .find_map(|(key, value)| key.contains("Sec-WebSocket-Key").then_some(value))
-            .unwrap();
-
-        let res = response(sec_key);
-
-        println!("Sending Responce:\n\n{res}");
-        stream.write_all(res.as_bytes())?;
-
-        // ----------------------------------------------------
-            // std::thread::sleep_ms(2000);
-            
-    }
-    Ok(())
-}
-
-pub fn response(key: &str) -> String {
-    use sha1::{Digest, Sha1};
-    let mut m = Sha1::new();
-    m.update(key.as_bytes());
-    m.update(b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11"); // Magic string
-    let key = base64::encode(m.finalize());
-
-    format!("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: {key}\r\n\r\n",)
-}
-
-pub fn apply_mask<const S: usize>(keys: [u8; S], payload: &mut [u8]) {
-    payload
-        .iter_mut()
-        .zip(keys.into_iter().cycle())
-        .for_each(|(p, m)| *p ^= m);
 }
 

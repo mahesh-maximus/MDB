c207643b6cbc4d5bb5801c4ee15a6c3cbf449946
Ubuntu <mahesh.ksl@gmail.com>
2022-12-06 16:58:49 +0000

    added threading

--- src/http_request_processor.rs
+++ src/http_request_processor.rs
@@ -1,3 +1,58 @@
+use std::{
+    fs,
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+    str,
+    io::{Read, Result, Write}
+};
+use std::thread::spawn;
+use tungstenite::accept;
+
 pub fn process_requests() {
-    println!("HTTP Processor <<>>");
+    println!("HTTP Request Processor <<>>");
+
+    thread::spawn(|| {
+
+    let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+    });
 }
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    
+    let baseFrontendPath = "/mdb/frontend/";
+    let a = format!("{}{}", baseFrontendPath, filename);
+    println!("zzzzzzzzzzzzzzzzzzz : {}", a);
+    let contents = fs::read_to_string(a).unwrap();
+    
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();  
+}
+
--- src/main.rs
+++ src/main.rs
@@ -14,93 +14,16 @@ mod ws_request_processor;
 fn main() {
 
     println!("Starting MDB ...");
-/*
-    thread::spawn(|| {
-      //  let listener = TcpListener::bind("0.0.0.0:8000").unwrap();
-
-    let server = TcpListener::bind("0.0.0.0:8000").unwrap();
-    for stream in server.incoming() {
-        spawn (move || {
-            let mut websocket = accept(stream.unwrap()).unwrap();
-            loop {
-                let msg = websocket.read_message().unwrap();
-
-                // We do not want to send back ping/pong messages.
-                if msg.is_binary() || msg.is_text() {
-                    websocket.write_message(msg).unwrap();
-                }
-            }
-        });
-    }
-
-   });
-
-*/
 
     ws_request_processor::process_requests();
 
     http_request_processor::process_requests();
 
-    println!("Starting WS");
+    println!("Started MDB");
 
-    let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
-    
-}
-
-fn handle_connection(mut stream: TcpStream) {
-   /*
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("/mdb/frontend/index.html").unwrap();
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-
-        stream.write_all(response.as_bytes()).unwrap();
-    // --snip--
-    } else {
-        println!("Not GET");
-    }*/
-
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else if buffer.starts_with(sleep) {
+    loop {
+        println!("Main Thread is waiting ...");
         thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
-    
-    let baseFrontendPath = "/mdb/frontend/";
-    let a = format!("{}{}", baseFrontendPath, filename);
-    println!("zzzzzzzzzzzzzzzzzzz : {}", a);
-    let contents = fs::read_to_string(a).unwrap();
-    
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
+    }
 
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();  
 }
-

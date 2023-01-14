6fcee6acf2c0549ab7dff26fa1bcac087ff51f06
Ubuntu <mahesh@mahesh-dev-01.yfghpgauam4uhaa44h2xrpc1ob.bx.internal.cloudapp.net>
2023-01-14 17:49:34 +0000

    fix micro-http path issue

--- src/http_server/src/lib.rs
+++ src/http_server/src/lib.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 
+
 pub struct HttpServer {
     
 }
--- src/mdb/src/main.rs
+++ src/mdb/src/main.rs
@@ -2,11 +2,39 @@ use mdbm::MdbExitCode;
 use std::env::{args, Args};
 use std::thread;
 use std::time::Duration;
+use std::{io, panic, process};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 mod http_server_adapter;
 
 fn main_exitable() -> MdbExitCode {
+    // Start firecracker by setting up a panic hook, which will be called before
+    // terminating as we're building with panic = "abort".
+    // It's worth noting that the abort is caused by sending a SIG_ABORT signal to the process.
+    panic::set_hook(Box::new(move |info| {
+        // We're currently using the closure parameter, which is a &PanicInfo, for printing the
+        // origin of the panic, including the payload passed to panic! and the source code location
+        // from which the panic originated.
+        println!("MDB {}", info);
+    }));
+
     http_server_adapter::run_http_server("main.py".to_string());
+
+    let term = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term));
+    while !term.load(Ordering::Relaxed) {
+        println!("Main Thread is waiting ...");
+        thread::sleep(Duration::from_secs(1));
+    }
+
+
+
+
+    println!("Stopping MDB");
+
+
+
     MdbExitCode::Ok
 }
 
@@ -22,13 +50,6 @@ fn main() {
         );
     }
 
-    main_exitable();
-
-    println!("Started MDB");
-
-    // Hold on to dear life, don't let the main method exit.
-    loop {
-        println!("Main Thread is waiting ...");
-        thread::sleep(Duration::from_secs(5));
-    }
+    let exit_code = main_exitable();
+    std::process::exit(exit_code as i32);
 }

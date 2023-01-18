d91ce05ba6a4113809aa753b9b243e9f94dfa591
Ubuntu <mahesh@mahesh-dev-01.yfghpgauam4uhaa44h2xrpc1ob.bx.internal.cloudapp.net>
2023-01-18 18:36:12 +0000

    basic http request and response

--- src/web_server/src/lib.rs
+++ src/web_server/src/lib.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use micro_http::{HttpServer, Response, StatusCode, Body, Version};
 
 pub struct WebServer {
     temp: String,
@@ -6,19 +7,30 @@ pub struct WebServer {
 
 impl WebServer {
     pub fn new(temp: String) -> Self {
-        Self {
-            temp,
-        }
+        Self { temp }
     }
 
-    pub fn bind_and_run(
-        &mut self,
-        path: &PathBuf
-    ) {
+    pub fn bind_and_run(&mut self, path: &PathBuf) {
         println!("{}", self.temp.to_string());
         println!("Unix socket path: {}", path.display());
 
+        std::fs::remove_file(path).unwrap_or_default();
 
+        let mut server = HttpServer::new(path).unwrap();
+        server.start_server().unwrap();
+        println!("Started micro http server");
+        loop {
+            for request in server.requests().unwrap() {
+                println!("Request received");
+                let response = request.process(|request| {
+                    let mut response = Response::new(Version::Http11, StatusCode::OK);
+                    let response_body = b"response body";
+                    response.set_body(Body::new(response_body.to_vec()));
+                    response
+                });
+                server.respond(response);
+                println!("Sent response to client");
+            }
+        }
     }
 }
-
--- tcp-proxy/main.py
+++ tcp-proxy/main.py
@@ -1,22 +1,20 @@
-import argparse
 import socket
 import sys
 from _thread import *
 
-
 max_connection = 5
 buffer_size = 8192
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 3000              # Arbitrary non-privileged port
+HOST = ''              
+PORT = 3000              
 
 def start():
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((HOST, PORT))
         sock.listen(1)
-        print("[*] Server started successfully")
+        print("HTTP Proxy-> TCP Socket started successfully.")
     except Exception:
-        print("[*] Unable to Initialize Socket")
+        print("HTTP Proxy-> Unable to Initialize Socket.")
         print(Exception)
         sys.exit(2)
 
@@ -25,44 +23,45 @@ def start():
     while True:
         try:
             conn, addr = sock.accept() #Accept connection from client browser
+            print ('HTTP Proxy-> Client request accepted from: {}.'.format(addr))
             data = conn.recv(buffer_size) #Recieve client data
-            print ("RECV DATA >>>>>>>>>>>>>>>>>>>>>>>");
-            start_new_thread(conn_string, (conn,data, addr)) #Starting a thread
+            print ('HTTP Proxy-> Received request {}.'.format(data))
+            start_new_thread(conn_string, (conn,data)) #Starting a thread
         except KeyboardInterrupt:
             sock.close()
-            print("\n[*] Graceful Shutdown")
+            print("HTTP Proxy-> Graceful Shutdown.")
             sys.exit(1)
 
-def conn_string(conn, data, addr):
+def conn_string(conn, data):
     try:
-        print(data)
-        proxy_server(conn, addr, data)
+        proxy_server(conn, data)
     except Exception:
-        print ("Con ERRROR >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        print (Exception)
+        print ('HTTP Proxy-> Exception:{}.'.format(Exception))
         pass
 
-def proxy_server( conn, addr, data):
+def proxy_server( conn, data):
     try:
-        print("proxy_server")
+        print("HTTP Proxy-> Connecting to Unix socket")
         sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         sock.connect("/tmp/mdb.socket")
+        print("HTTP Proxy-> Connected to Unix socket")
         sock.send(data)
+        print("HTTP Proxy-> Sent data to Unix socket")
 
         while 1:
-            print ("Reading UNIX")
+            print ("HTTP Proxy-> Waiting to read data from Unix socket.")
             reply = sock.recv(4096)
-            print ("UNIX reply ________")
-            print (reply)
-            if(len(reply)>0):
+            print ('HTTP Proxy-> Rreceived data from Unix socket:{}.'.format(reply))
+            if(len(reply) > 0):
                 conn.send(reply)
+                print ("HTTP Proxy-> Sent data received from Unix socket to client")
             else:
+                print ("HTTP Proxy-> Completed sending data.")
                 break
-                print ("break")
         sock.close()
         conn.close()
     except socket.error:
-        print ("Proxy error >>>>>>>>>>>>>>>>>>>>>>");
+        print ('HTTP Proxy-> Socket error:{}.'.format(socket.error))
         sock.close()
         conn.close()
         print(sock.error)

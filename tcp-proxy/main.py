9d20db04fedfc508882cb571b7337296fe0464d6
Ubuntu <mahesh@mahesh-dev-01.yfghpgauam4uhaa44h2xrpc1ob.bx.internal.cloudapp.net>
2023-01-19 18:59:12 +0000

    introduce color print

--- tcp-proxy/main.py
+++ tcp-proxy/main.py
@@ -12,10 +12,10 @@ def start():
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((HOST, PORT))
         sock.listen(1)
-        print("HTTP Proxy-> TCP Socket started successfully.")
+        printc('1','44', '1', "HTTP Proxy-> TCP Socket started successfully.")
     except Exception:
-        print("HTTP Proxy-> Unable to Initialize Socket.")
-        print(Exception)
+        printc('1','44', '1', "HTTP Proxy-> Unable to Initialize Socket.")
+        printc('1','44', '1', Exception)
         sys.exit(2)
 
 
@@ -23,49 +23,52 @@ def start():
     while True:
         try:
             conn, addr = sock.accept() #Accept connection from client browser
-            print ('HTTP Proxy-> Client request accepted from: {}.'.format(addr))
+            printc('1','44', '1', 'HTTP Proxy-> Client request accepted from: {}.'.format(addr))
             data = conn.recv(buffer_size) #Recieve client data
-            print ('HTTP Proxy-> Received request {}.'.format(data))
+            printc('1','44', '1', 'HTTP Proxy-> Received request {}.'.format(data))
             start_new_thread(conn_string, (conn,data)) #Starting a thread
         except KeyboardInterrupt:
             sock.close()
-            print("HTTP Proxy-> Graceful Shutdown.")
+            printc('1','44', '1', "HTTP Proxy-> Graceful Shutdown.")
             sys.exit(1)
 
 def conn_string(conn, data):
     try:
         proxy_server(conn, data)
     except Exception:
-        print ('HTTP Proxy-> Exception:{}.'.format(Exception))
+        printc('1','44', '1', 'HTTP Proxy-> Exception:{}.'.format(Exception))
         pass
 
 def proxy_server( conn, data):
     try:
-        print("HTTP Proxy-> Connecting to Unix socket")
+        printc('1','44', '1', "HTTP Proxy-> Connecting to Unix socket")
         sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         sock.connect("/tmp/mdb.socket")
-        print("HTTP Proxy-> Connected to Unix socket")
+        printc('1','44', '1', "HTTP Proxy-> Connected to Unix socket")
         sock.send(data)
-        print("HTTP Proxy-> Sent data to Unix socket")
+        printc('1','44', '1', "HTTP Proxy-> Sent data to Unix socket")
 
         while 1:
-            print ("HTTP Proxy-> Waiting to read data from Unix socket.")
-            reply = sock.recv(4096)
-            print ('HTTP Proxy-> Rreceived data from Unix socket:{}.'.format(reply))
+            printc('1','44', '1', "HTTP Proxy-> Waiting to read data from Unix socket.")
+            reply = sock.recv(8192)
+            printc('1','44', '1', 'HTTP Proxy-> Rreceived data from Unix socket:{}.'.format(reply))
             if(len(reply) > 0):
                 conn.send(reply)
-                print ("HTTP Proxy-> Sent data received from Unix socket to client")
+                printc('1','44', '1', "HTTP Proxy-> Sent data received from Unix socket to client")
             else:
-                print ("HTTP Proxy-> Completed sending data.")
+                printc('1','44', '1', "HTTP Proxy-> Completed sending data.")
                 break
         sock.close()
         conn.close()
     except socket.error:
-        print ('HTTP Proxy-> Socket error:{}.'.format(socket.error))
+        printc('1','44', '1', 'HTTP Proxy-> Socket error:{}.'.format(socket.error))
         sock.close()
         conn.close()
-        print(sock.error)
+        printc('1','44', '1', sock.error)
         sys.exit(1)
+
+def printc(style, fg, bg, msg):
+    print('\x1b[{};{};{}m'.format(style, fg, bg) + msg + '\x1b[0m') 
 
 if __name__== "__main__":
     start()

86dd4ac1d1dc5820a45eb2b0e5c209b2eb32066d
Ubuntu <mahesh@mahesh-dev-01.yfghpgauam4uhaa44h2xrpc1ob.bx.internal.cloudapp.net>
2023-02-01 19:40:36 +0000

    start unit tests and mutex web socket server.

--- src/mdb/src/ws_server_adapter.rs
+++ src/mdb/src/ws_server_adapter.rs
@@ -1,12 +1,12 @@
 use mdbm::MdbExitCode;
-use ws_server::WS_SERVER;
+//use ws_server::WS_SERVER;
 
 
 
 pub fn run_ws_server() -> MdbExitCode {
     println!("run_ws_server fn");
 
-    WS_SERVER.lock().unwrap().bind_and_run("0.0.0.0:8000".to_string());
+    //WS_SERVER.lock().unwrap().bind_and_run("0.0.0.0:8000".to_string());
     println!("run_ws_server fn -----------");
 
 
--- src/ws-server/src/lib.rs
+++ src/ws-server/src/lib.rs
@@ -1,48 +1,152 @@
 use lazy_static::lazy_static;
+
+use std::collections::VecDeque;
 use std::thread::{self, spawn};
-use std::{net::TcpListener, sync::Mutex};
-use tungstenite::accept;
+use std::{net::TcpListener, sync::Mutex, time};
+use tungstenite::{accept, Message};
 
 //https://users.rust-lang.org/t/global-mutable-variables-in-rust/77056
 //https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=820f36fe2c8faf6303a2b567faf47ac8
 lazy_static! {
-    pub static ref WS_SERVER: Mutex<WebSocketServer> = {
-        let ws_server = WebSocketServer::new(false);
+    static ref WS_SERVER: Mutex<WebSocketServerInner> = {
+        let ws_server = WebSocketServerInner::new();
         Mutex::new(ws_server)
+    };
+
+    static ref SEND_QUEUE: Mutex<VecDeque<String>> = {
+        let send_queue = VecDeque::new();
+        Mutex::new(send_queue)
     };
 }
 
-pub struct WebSocketServer {
-    example: bool,
+pub enum MessageTye {
+    Ping,
+    LiveReload,
 }
 
+pub struct WebSocketMessage {
+    message_type: MessageTye,
+    body: String,
+
+}
+
+pub struct WebSocketServer {}
+
 impl WebSocketServer {
-    pub fn new(example: bool) -> Self {
-        println!("WebSocketServer.new fn .");
-        WebSocketServer { example }
+
+    pub fn new() -> Self {
+        Self {}
     }
 
     pub fn bind_and_run(&mut self, address: String) {
-        println!(
-            "WebSocketServer.bind_and_run fn {}.",
-            self.example.to_string()
-        );
+        WS_SERVER.lock().unwrap().bind_and_run(address);
+    }
+
+    pub fn write_message(&mut self, message: String) {
+        WS_SERVER.lock().unwrap().write_message(message);
+    }
+
+    pub fn get_send_queue_len(&mut self) -> usize {
+        WS_SERVER.lock().unwrap().get_send_queue_len()
+    }
+}
+
+struct WebSocketServerInner {}
+
+impl WebSocketServerInner {
+    pub fn new() -> Self {
+        println!("WebSocketServer.new fn .");
+        WebSocketServerInner {}
+    }
+
+    fn bind_and_run(&mut self, address: String) {
+        println!("WebSocketServer.bind_and_run fn.");
+
 
         thread::spawn(move || {
             let server = TcpListener::bind(address.to_string()).unwrap();
             println!("WebSocketServer.bind_and_run fn waiting for incoming ...");
+
+            
             for stream in server.incoming() {
+
                 spawn(move || {
                     let mut websocket = accept(stream.unwrap()).unwrap();
+                    
                     loop {
-                        let msg = websocket.read_message().unwrap();
 
-                        if msg.is_binary() || msg.is_text() {
-                            websocket.write_message(msg).unwrap();
+                        while SEND_QUEUE.lock().unwrap().len() > 0 {
+                            let message = SEND_QUEUE.lock().unwrap().pop_front().unwrap().to_string();
+
+                            println!("WebSocketServer.bind_and_run, try to send message: '{}' to client", message);
+                            websocket.write_message(Message::Text(message)).unwrap();
+                            println!("WebSocketServer.bind_and_run, message sent to client.");
                         }
+
+                        
+                        thread::sleep(time::Duration::from_millis(100));
+
+                        //let msg = websocket.read_message().unwrap();
+
+                        // if msg.is_binary() || msg.is_text() {
+                        //     websocket.write_message(msg).unwrap();
+                        // }
                     }
                 });
             }
         });
+    }
+
+    pub fn get_send_queue_len(&mut self) -> usize {
+        SEND_QUEUE.lock().unwrap().len()
+    }
+
+    pub fn write_message(&mut self, message: String) {
+        println!("WebSocketServer.write_message fn.");
+        SEND_QUEUE.lock().unwrap().push_back(message);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{thread, time};
+
+    use tungstenite::{connect, Message};
+    use url::Url;
+
+    use crate::WebSocketServer;
+
+    #[test]
+    fn xyz() {
+        let mut ws = WebSocketServer::new();
+        ws.bind_and_run("localhost:7777".to_string());
+
+        thread::sleep(time::Duration::from_millis(100));
+
+        let (mut socket, response) =
+        connect(Url::parse("ws://localhost:7777").unwrap()).expect("Can't connect WS server.");
+
+        println!("Connected to the WS server");
+        println!("Response HTTP code: {}", response.status());
+        println!("Response contains the following headers:");
+        for (ref header, _value) in response.headers() {
+            println!("* {}", header);
+        }
+
+        thread::sleep(time::Duration::from_secs(1));
+
+        ws.write_message("Message from WS client".to_string());
+
+        println!("_________________ : {}", ws.get_send_queue_len().to_string());
+
+        thread::sleep(time::Duration::from_secs(1));
+
+
+        let msg = socket.read_message().expect("Error reading message");
+
+        println!("Received******************************: {}", msg);
+
+        thread::sleep(time::Duration::from_secs(1));
+
     }
 }
